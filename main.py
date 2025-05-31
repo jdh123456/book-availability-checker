6c0eb81dfc36a1ae91289d34373b9ee6522466f5
jdh123456 <atlas_vulture8f@icloud.com>
2025-05-31 17:39:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -55,4 +55,5 @@ def check_availability():
     return jsonify(results if results else {"message": "No results found."})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000)
+    port = int(os.environ.get("PORT", 3000))
+    app.run(host="0.0.0.0", port=port)
